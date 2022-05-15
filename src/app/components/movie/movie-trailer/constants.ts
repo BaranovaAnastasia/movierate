e7a857fdfbebb9bf9ffac20e6ea3364789a6d7ca
BaranovaAnastasia <aabaranova_3@edu.hse.ru>
2022-05-16 02:29:22 +0300

    fix: trailer link changed to https

--- src/app/components/movie/movie-trailer/constants.ts
+++ src/app/components/movie/movie-trailer/constants.ts
@@ -1,3 +1,3 @@
-export const TRAILER_URL = 'http://www.youtube.com/embed/';
+export const TRAILER_URL = 'https://www.youtube.com/embed/';
 export const TRAILER_PREVIEW_URL = 'https://img.youtube.com/vi/';
 export const TRAILER_PREVIEW_QUALITY_URL = '/maxresdefault.jpg';
